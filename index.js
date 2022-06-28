6576ab3f9f141d8714f5c8c8c01aa738ef15fe3f
Nico <nicolasbusso@gmail.com>
2022-06-28 19:50:57 -0300

    quita de comentarios

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 // Calcular Retenciones en un pago a proveedores
 
-//Pedirle al usuario que nos indique cuantas facturas tiene que pagar y asignando un máximo de 10
 let cantFacturas = parseInt(prompt('Ingrese la cantidad de facturas a pagar. (Máximo 10)'))
 
-//no permitimos que el usuario ingrese datos no validos
 while(cantFacturas > 10 || cantFacturas < 1){
     alert('Ingresa una cantidad correcta (de 1 a 10)')
     cantFacturas = parseInt(prompt('Ingrese la cantidad de facturas a pagar. (Máximo 10)'))
 }
 
-//declaramos la variable de importe total
 let importeTotal = 0
 
-//Bucle para que nos pida el valor de cada factura
 for (let i = 1; i <= cantFacturas; i++){
     let importe = parseFloat(prompt(`Usted está pagando ${cantFacturas} facturas.
                                     \nEl valor total ingresado hasta el momento es de: $${importeTotal}.
@@ -20,7 +16,6 @@ for (let i = 1; i <= cantFacturas; i++){
     importeTotal = parseFloat(importeTotal + importe)
 }
 
-//Pedirle al usuario que nos indique que tipo de IVA manejan sus facturas
 let tipoIva = 0
 let seleccionIva = parseInt(prompt(`Usted está pagando ${cantFacturas} facturas.
                                     \nEl valor total ingresado hasta el momento es de: $${importeTotal}.
@@ -31,7 +26,6 @@ let seleccionIva = parseInt(prompt(`Usted está pagando ${cantFacturas} facturas
                                     \n3. Servicios (27%)`)
                                     )   
 
-//no permitimos que el usuario ingrese datos no validos
 while(seleccionIva > 3 || seleccionIva < 1){
     alert('Selecciona un tipo de iva correcto!')
     seleccionIva = parseInt(prompt(`Usted está pagando ${cantFacturas} facturas.
@@ -70,11 +64,8 @@ function retGanancias(monto, iva){
     return retencion
 }
 
-//asignamos variables a los resultados de las retenciones
 let iibb = retBrutos(importeTotal)
 let gcias = retGanancias(importeTotal, tipoIva)
-
-//decidimos mostrar 0 si la retencion no supera los valores minimos ($280 para iibb y $500 para ganancias)
 
 function mostrar(ret, minimo){
     let retencion = 0
@@ -84,10 +75,8 @@ function mostrar(ret, minimo){
     return retencion
 }
 
-//asignamos los valores que deberá pagar el cliente descontando las retenciones
 let valores = importeTotal - mostrar(iibb, 280) - mostrar(gcias, 500)
 
-//mostramos como el usuario debería formular su pago de acuerdo a las retenciones
 alert(`La suma de sus ${cantFacturas} facturas es de $${importeTotal}. (IVA ${tipoIva * 100}%)
         \n-----
         \nDeberá Formular el pago de la siguiente manera:
@@ -97,5 +86,3 @@ alert(`La suma de sus ${cantFacturas} facturas es de $${importeTotal}. (IVA ${ti
         \n-----
         \nGracias por su consulta.
 `)
-
-//fin
